76dbd93f400e766379a75a899daa7f197269cfe6
nithin3119@gmail.com <nithin3119@gmail.com>
2023-04-09 23:20:09 +0530

    f

---
+++ src/Components/RowPost/RowPost.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import './RowPost.css'
+import { useEffect,useState } from 'react'
+import axios from 'axios'
+import {API_KEY,imageUrl} from '../../constants/constants'
+
+function RowPost(props) {
+    const [movies, setMovies] = useState([]);
+    
+    // useEffect(() => {
+    //   axios.get(`discover/tv?api_key=${API_KEY}&with_networks=213`)
+    //     .then((response) => {
+    //       console.log(response.data);
+    //       setMovies(response.data.results);
+    //     })
+    //     .catch(err => {
+    //       alert("Error fetching movies");
+    //     });
+    // }, []);
+    useEffect(()=>{
+      axios.get(props.url).then((response)=>{
+          console.log(response.data);
+      setMovies(response.data.results)
+      }).catch(err=>{
+          alert('Network error')
+      })
+          },[]);
+         
+    return (
+      <div className='row'>
+      <h2>{props.title}</h2>
+      <div className='posters'>
+          {movies.map((obj)=>
+              
+              <img  className={props.isSmall ? 'smallPoster' : 'poster'} alt='poster' src={`${imageUrl+obj.backdrop_path}`} />
+          
+          )}
+          
+      </div>
+     {/* {urlid && <Youtube opts ={opts} videoId={urlid.key}/>} */}
+  </div>
+    );
+  }
+  
+export default RowPost
